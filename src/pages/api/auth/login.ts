f56124bd32d840e39ccee4c33fb32f3c18fd89cf
Sebastian <skov@skxv.dev>
2024-07-12 16:13:11 +0000

    Method

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -8,34 +8,36 @@ import jwt from "jsonwebtoken";
 connect();
 
 export default async function handler(req, res) {
-  try {
-    const { email, password, remember } = req.body;
+  if ((req.method = "POST")) {
+    try {
+      const { email, password, remember } = req.body;
 
-    const user = await User.findOne({ email });
+      const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(400).json({ message: "User doesn't exist" });
+      if (!user) {
+        return res.status(400).json({ message: "User doesn't exist" });
+      }
+
+      const validPassword = await bcryptjs.compare(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: "Invalid password" });
+      }
+
+      const tokenData = {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      };
+
+      const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {
+        expiresIn: remember ? "365d" : "1d",
+      });
+
+      return res.status(200).json({ message: "Successfully logged in", token });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: err });
     }
-
-    const validPassword = await bcryptjs.compare(password, user.password);
-    if (!validPassword) {
-      return res.status(400).json({ message: "Invalid password" });
-    }
-
-    const tokenData = {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-    };
-
-    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {
-      expiresIn: remember ? "365d" : "1d",
-    });
-
-    return res.status(200).json({ message: "Successfully logged in", token });
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: "Internal server error", error: err });
   }
 }
