58c0c08286c4de94a16cc484add1368d9e3bd244
Sebastian Skov Nielsen <56512071+DDDASHXD@users.noreply.github.com>
2024-07-20 17:33:27 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApplicationProvider } from "@/providers/ApplicationProvider";
+import Update from "./update";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ApplicationProvider>
         <Component {...pageProps} />
         <Toaster />
+        <Update />
       </ApplicationProvider>
     </>
   );
